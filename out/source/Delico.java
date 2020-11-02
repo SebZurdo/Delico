8a6df8339775bf559d8cb1a96fdad23a92154f8e
SebZurdo <seflorez2003@gmail.com>
2020-11-02 17:18:38 -0500

    Tesssst

--- out/source/Delico.java
+++ out/source/Delico.java
@@ -14,63 +14,14 @@ import java.io.IOException;
 
 public class Delico extends PApplet {
 
-int x = 25,y = 25;
 public void setup() {
     
 }
 
 public void draw() {
-    background(255);
-    stroke(0);
-    strokeWeight(1.2f);
-    fill(0);
-    text("x ="+str(x),350,40);
-    text("y ="+str(y),350,60);
-    fill(255);
-    rect(225,225,450,450);
-    rect(x,y,25,25);
-    line(0, 225, 225, 225);
-    fill(0);
-    line(600, 225, 900, 225);
-    line(0, 675, 225, 675);
-    line(600, 675, 900, 675);
-    fill(255,0,0);
-    line(225, 225, 600, 225);
-    line(225, 675, 600, 675);
+    rect(40,40,40,40);
 }
-
-public void keyPressed() {
-    if(key == CODED){
-        if(keyCode == RIGHT){
-            mover("DER");
-        }
-        if(keyCode == LEFT){
-            mover("IZQ");
-        }
-        if(keyCode == DOWN){
-            mover("DOWN");
-        }
-        if(keyCode == UP){
-            mover("UP");
-        }
-    }
-}
-public void mover(String dir){
-    if(dir == "DER"){
-        x =x+25;
-    }
-    if(dir == "IZQ"){
-        x =x-25;
-    }
-    if(dir == "DOWN"){
-        y =y + 25;
-    }
-    if(dir == "UP"){
-        y =y - 25;
-    }
-  
-}
-  public void settings() {  size(900,900); }
+  public void settings() {  size(1600,960); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Delico" };
     if (passedArgs != null) {
